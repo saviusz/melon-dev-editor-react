62a714f86c868bcb4dbd1aabd2d83ac42b7070a7
Saviusz <serweryn123@gmail.com>
2023-10-07 19:03:51 +0200

    Add: Paragraph addition & deletion

--- src/Components/EditorParagraph/index.tsx
+++ src/Components/EditorParagraph/index.tsx
@@ -1,21 +1,34 @@
-import { FormEvent, KeyboardEvent } from "react";
+import { FormEvent, KeyboardEvent, MutableRefObject, useRef } from "react";
 import "./style.css";
 
 export interface Params {
   content: string;
   onDelete: () => void;
   onChangeFocus: (direction: number) => void;
+  onParagraphAdd: () => void;
+  outRef: (elem: HTMLParagraphElement | null) => void;
 }
 
-function EditorParagraph({ content, onDelete, onChangeFocus }: Params) {
-  function handleRightArrow(e: KeyboardEvent) {}
-  function handleBackspace(e: KeyboardEvent) {
-    if (content.length == 0) {
+function EditorParagraph({
+  content,
+  onDelete,
+  onChangeFocus,
+  outRef,
+  onParagraphAdd,
+}: Params) {
+  const p = useRef<HTMLParagraphElement | null>(null);
+
+  const handleRightArrow = (e: KeyboardEvent) => {};
+  const handleBackspace = (e: KeyboardEvent) => {
+    if (checkCurrentLength() == 0) {
       e.preventDefault();
-      onChangeFocus(-1);
       onDelete();
     }
-  }
+  };
+
+  const checkCurrentLength = () => {
+    return p.current?.innerText.replace("\n", "").length;
+  };
 
   function handleKeydown(event: KeyboardEvent) {
     switch (event.key) {
@@ -23,6 +36,9 @@ function EditorParagraph({ content, onDelete, onChangeFocus }: Params) {
         return handleBackspace(event);
       case "RightArrow":
         return handleRightArrow(event);
+      case "a":
+        event.preventDefault();
+        return onParagraphAdd();
     }
   }
   function handleInput(event: FormEvent<HTMLParagraphElement>) {}
@@ -31,8 +47,13 @@ function EditorParagraph({ content, onDelete, onChangeFocus }: Params) {
     <p
       className="editor-paragraph"
       contentEditable="true"
+      suppressContentEditableWarning={true}
       onKeyDown={handleKeydown}
       onInput={handleInput}
+      ref={(elem) => {
+        outRef(elem);
+        p.current = elem;
+      }}
     >
       {content}
     </p>
--- src/Components/SongEditor/index.tsx
+++ src/Components/SongEditor/index.tsx
@@ -1,13 +1,37 @@
 import "./style.css";
 import EditorParagraph from "../EditorParagraph";
 import { useImmer } from "use-immer";
+import { useEffect, useRef, useState } from "react";
 
 function SongEditor() {
   const [content, updateContent] = useImmer(["line 1", "line 2"]);
+  const paragraphs = useRef<Array<HTMLParagraphElement>>([]);
+  const [focus, setFocus] = useState<number | undefined>(undefined);
 
   const handleParagraphDeletion = (index: number) => {
-    updateContent(content.filter((_value, i) => i != index));
+    if (content.length > 1) {
+      updateContent(content.filter((_value, i) => i != index));
+      handleFocusChange(index - 1);
+    }
   };
+
+  const handleParagraphAddition = () => {
+    updateContent((draft) => {
+      draft.push("");
+    });
+    handleFocusChange(content.length);
+  };
+
+  const handleFocusChange = (index: number) => {
+    setFocus(index);
+  };
+
+  useEffect(() => {
+    if (focus) {
+      paragraphs.current[focus].focus();
+      setFocus(undefined);
+    }
+  }, [focus]);
 
   return (
     <div className="song-editor">
@@ -15,7 +39,12 @@ function SongEditor() {
         <EditorParagraph
           key={index}
           content={p}
+          onChangeFocus={(dir: number) => handleFocusChange(index + dir)}
           onDelete={() => handleParagraphDeletion(index)}
+          onParagraphAdd={() => handleParagraphAddition()}
+          outRef={(elem: HTMLParagraphElement) => {
+            paragraphs.current[index] = elem;
+          }}
         />
       ))}
     </div>
